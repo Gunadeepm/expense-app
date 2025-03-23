949c436b9611fec8c5fd55e5d081932358688e4e
guna <gunadeeprakeshm@gmail.com>
2025-03-23 23:14:08 +0530

    autopush

--- 1.frontend.sh
+++ 1.frontend.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 component=frontend
-webserver=nginx
 source common.sh
 
 #changing the ec2-instance's name
 set-hostname $component
 
 #installing nginx webserver on our instance
-echo -n "Installing $webserver:"
-dnf install $webserver -y &>> /tmp/$component.log
+echo -n "Installing nginx:"
+dnf install nginx -y &>> /tmp/$component.log
 status
 
 #starting and enbaling the nginx
-echo -n "Starting the $webserver:"
-systemctl enable $webserver  &>> /tmp/$component.log
-systemctl start $webserver  &>> /tmp/$component.log
+echo -n "Starting the nginx:"
+systemctl enable nginx  &>> /tmp/$component.log
+systemctl start nginx  &>> /tmp/$component.log
 status
 
 #Removing the default/cached content of nginx
 echo -n "removing the bydefault content:"
-rm -rf /usr/share/$webserver/html/*  
+rm -rf /usr/share/nginx/html/*  
 status
 
 #downloading the web-content for frontend
@@ -29,12 +28,12 @@ status
 
 #extracting the frontend content 
 echo -n "Extracting the content for $component:"
-cd /usr/share/$webserver/html 
+cd /usr/share/nginx/html 
 unzip -o /tmp/$component.zip  &>> /tmp/$component.log
 status
 
 #restarting the nginx
-echo -n "Restarting the $webserver"
-systemctl enable $webserver
-systemctl restart $webserver
+echo -n "Restarting the nginx"
+systemctl enable nginx
+systemctl restart nginx
 status
