4182551858dc7e01f55cf6efd5fa195590639866
guna <gunadeeprakeshm@gmail.com>
2025-03-24 00:13:01 +0530

    autopush

--- common.sh
+++ common.sh
@@ -1,15 +1,15 @@
 #!bin/bash
-root() {
-    if [ $(id -u) -ne 0]; then
-        echo -e "\e[31m You should have the root access to perform this task! \e[0m"
-    fi 
-}
+# root() {
+#     if [ $(id -u) -ne 0]; then
+#         echo -e "\e[31m You should have the root access to perform this task! \e[0m"
+#     fi 
+# }
 
-status() {
-    if [ $? -eq 0 ]; then 
-        echo -e "\e[32m Sucess \e[0m"
-    else
-        echo -e "\e[31m Failure \e[0m"
-    fi
-}
+# status() {
+#     if [ $? -eq 0 ]; then 
+#         echo -e "\e[32m Sucess \e[0m"
+#     else
+#         echo -e "\e[31m Failure \e[0m"
+#     fi
+# }
 
