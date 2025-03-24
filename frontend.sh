b71d1ed0be5c6e45b691a4421b05829d3969720d
guna <gunadeeprakeshm@gmail.com>
2025-03-24 11:20:19 +0530

    autopush

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-set -x 
+component=frontend
+logs=/tmp/frontend.log
+source common.sh
+
+set -x #--> debug mode
 
 #changing the ec2-instance's name
 set-hostname Frontend-Server
 
 #installing nginx webserver on our instance
 echo -n "Installing nginx:"
-dnf install nginx -y  
+dnf install nginx -y  &>> $logs
 if [ $? -eq 0 ]; then 
         echo -e "\e[32m Sucess \e[0m"
     else
@@ -15,8 +19,8 @@ fi
 
 #starting and enbaling the nginx
 echo -n "Starting the nginx:"
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx  &>> $logs
+systemctl start nginx   &>> $logs
 status
 
 #Removing the default/cached content of nginx
@@ -26,17 +30,17 @@ status
 
 #downloading the web-content for frontend
 echo -n "downloading the artifactory:"
-curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip  &>> $logs
 status
 
 #extracting the frontend content 
 echo -n "Extracting the content for frontend:"
 cd /usr/share/nginx/html 
-unzip -o /tmp/frontend.zip  
+unzip -o /tmp/frontend.zip  &>> $logs
 status
 
 #restarting the nginx
 echo -n "Restarting the nginx"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx  &>> $logs
+systemctl restart nginx &>> $logs
 status
