4d15750944b982f04c36070c1b6b6985331f5f13
guna <gunadeeprakeshm@gmail.com>
2025-03-24 10:56:48 +0530

    autopush

--- frontend.sh
+++ frontend.sh
@@ -12,30 +12,30 @@ if [ $? -eq 0 ]; then
         echo -e "\e[31m Failure \e[0m"
 fi 
 
-# #starting and enbaling the nginx
-# echo -n "Starting the nginx:"
-# systemctl enable nginx  &>> /tmp/$component.log
-# systemctl start nginx  &>> /tmp/$component.log
-# status
+#starting and enbaling the nginx
+echo -n "Starting the nginx:"
+systemctl enable nginx  &>> /tmp/$component.log
+systemctl start nginx  &>> /tmp/$component.log
+status
 
-# #Removing the default/cached content of nginx
-# echo -n "removing the bydefault content:"
-# rm -rf /usr/share/nginx/html/*  
-# status
+#Removing the default/cached content of nginx
+echo -n "removing the bydefault content:"
+rm -rf /usr/share/nginx/html/*  
+status
 
-# #downloading the web-content for frontend
-# echo -n "downloading the artifactory:"
-# curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/$component.zip &>> /tmp/$component.log
-# status
+#downloading the web-content for frontend
+echo -n "downloading the artifactory:"
+curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/$component.zip &>> /tmp/$component.log
+status
 
-# #extracting the frontend content 
-# echo -n "Extracting the content for $component:"
-# cd /usr/share/nginx/html 
-# unzip -o /tmp/$component.zip  &>> /tmp/$component.log
-# status
+#extracting the frontend content 
+echo -n "Extracting the content for $component:"
+cd /usr/share/nginx/html 
+unzip -o /tmp/$component.zip  &>> /tmp/$component.log
+status
 
-# #restarting the nginx
-# echo -n "Restarting the nginx"
-# systemctl enable nginx
-# systemctl restart nginx
-# status
+#restarting the nginx
+echo -n "Restarting the nginx"
+systemctl enable nginx
+systemctl restart nginx
+status
