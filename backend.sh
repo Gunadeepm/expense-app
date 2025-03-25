d2d59205e11eb70bd6e82c3b8e5f2bb930f3ac58
guna <gunadeeprakeshm@gmail.com>
2025-03-26 01:13:51 +0530

    settingup the backend

--- backend.sh
+++ backend.sh
@@ -7,19 +7,23 @@ source common.sh
 
 # Here, we're disabling the default node(16v) repo and enable the needed version 20 repo.
 echo -n "Configuring the nodejs20 repo:-"
-dnf module list                       &>>$logs
-dnf module disable $component -y          &>>$logs
-dnf module enable $component:20 -y        &>>$logs
+dnf module list                           &>> $logs
+dnf module disable $component -y          &>> $logs
+dnf module enable $component:20 -y        &>> $logs
 status $?
 
 # Now, we have completed the enabling the nodejs20 repository. we now have to download & install the nodejs20.
 echo -n "Installing $component:-"
-dnf install $component -y   &>>$logs
+dnf install $component -y   &>> $logs
+status $?
+
+echo -n "Configuring the systemd service:-"
+cp backend.service /etc/systemd/system/
 status $?
 
 # For configuring the application, we should create a user account for running the application. 
-echo -n "Creating an user_account: expense:-"
-useradd expense &>>$logs
+echo -n "Creating a service_account: expense:-"
+useradd expense &>> $logs
 status $?
 
 # we have to keep the application in a standard location
@@ -44,6 +48,26 @@ cd /app
 npm install   &>>$logs   #-->This will download and install all the packages needed for the application from package.json in a binary format under node_modules.
 status $?
 
+# Changing the permissions and ownership of the /app directory to "expense" service account.
+# All the objects belongs to application should be owned by application
+echo -n "Granting the necessary permissions & ownership:-"
+chmod -R 775 /app 
+chown -R expense:expense /app
+status $?
 
+# For this application to work fully functional we need to load schema to the Database.
+# Defining schema is like defining the structure of the table with rows & columns.
+# We need to load the schema. To load schema we need to install mysql client on this backend application instance.
+echo -n "Installing the mysql client:-"
+dnf install mysql-server -y &>> $logs
+status $?
 
+echo -n "Injecting the schema from the backend app:-"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>> $logs
+status $?
 
+echo -n "Loading and starting the service:-"
+systemctl daemon-reload  &>> $logs
+systemctl enable backend &>> $logs
+systemctl start backend  &>> $logs
+stat $? 
