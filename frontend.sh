b269b207e693a13ae157063ce6311892d4aa2b03
guna <gunadeeprakeshm@gmail.com>
2025-03-25 11:12:10 +0530

    auto push

--- frontend.sh
+++ frontend.sh
@@ -16,7 +16,7 @@ status $?
 
 # copying the reverse proxy file to its default location
 echo -n "Settingup the reverse proxy:"
-cp reverse proxy /etc/nginx/default.d/expense.conf &>> $logs
+cp reverse proxy.conf /etc/nginx/default.d/expense.conf &>> $logs
 status $?
 
 #Removing the default/cached content of nginx
