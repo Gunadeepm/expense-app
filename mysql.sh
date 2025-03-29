f1e3d48f8b57ef6384af24e394cd58551d9730d9
guna <gunadeeprakeshm@gmail.com>
2025-03-29 20:50:25 +0530

    settingup the backend

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,11 @@ component=mysql
 logs=/tmp/mysql.log
 source common.sh
 
+if [ -z $1 ]; then 
+    echo -e "\e[31m mysql root user password \e[0m"
+    echo -e "Example usage: \n\t \e[35m sudo bash $0 password \e[0m"
+    exit 1
+fi
 # mysql version=8.0.x
 echo -n "Installing $component server:"
 dnf install $component-server -y  &>>logs
@@ -17,6 +22,6 @@ status $?
 # Next, We need to change the default root password in order to start using the database service. 
 # Here, we have used password ExpenseApp@1
 echo -n "Configuring the database service:"
-mysql_secure_installation --set-root-pass ExpenseApp@1   &>>logs
+mysql_secure_installation --set-root-pass $1 &>>logs
 status $?
 
